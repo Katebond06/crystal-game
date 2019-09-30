4eb6a34313ee1bd4083052b275474803d45df521
kate <bkateryna27@icloud.com>
2019-09-30 09:58:54 -0400

    first push

---
+++ game.js
@@ -0,0 +1,13 @@
+// computer gives a random number;
+// asign value to each crystal;
+// user clicks on the crystal;
+// the total score updates;
+// check if user wins or losses
+// random numbers under crystals are going to update, after the game ends (user wins/losses);
+// wins and losses score updates;
+
+var random = Math.floor(Math.random() * 101) + 19
+console.log(random);
+$("#random").text(random);
+
+
